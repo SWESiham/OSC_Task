63aeb6502ddc2ab80377a636a9a874c905d01729
SWESiham <128804940+SWESiham@users.noreply.github.com>
2023-08-27 13:19:50 +0300

    calculator.sh

--- Session4/calculator.sh
+++ Session4/calculator.sh
@@ -4,45 +4,32 @@ echo "2. Subtraction"
 echo "3. Multiplication"
 echo "4. Division"
 echo "5. Exit"
-choice=$1
-c=0
-f=$2
-s=$3
-while
-echo "Enter your choice: "
-read choice
+while true;
+do
+read -p "Enter your choice: " choice
 if [[ $choice == 1 ]]
 then 
-echo "Enter the first number: "
-read f
-echo "Enter the second number: "
-read s 
+read -p "Enter the first number: " f
+read -p "Enter the second number: " s
 echo "$f + $s = " $(($f+$s))
 elif [[ $choice == 2 ]]
 then
-echo "Enter the first number: "
-read f 
-echo "Enter the second number: "
-read s 
+read -p "Enter the first number: " f
+read -p "Enter the second number: " s
 echo "$f - $s = " $(($f-$s))
 elif [[ $choice == 3 ]]
 then
-echo "Enter the first number: "
-read f 
-echo "Enter the second number: "
-read s 
+read -p "Enter the first number: " f
+read -p "Enter the second number: " s
 echo "$f * $s = " $(($f*$s))
 elif [[ $choice == 4 ]]
 then 
-echo "Enter the first number: "
-read f 
-echo "Enter the second number: "
-read s 
+read -p "Enter the first number: " f
+read -p "Enter the second number: " s
 echo "$f / $s = "  $(expr $f / $s) 
 elif [[ $choice == 5 ]]
 then 
 echo "Exiting..."
 break
 fi
-do c==0;
 done
